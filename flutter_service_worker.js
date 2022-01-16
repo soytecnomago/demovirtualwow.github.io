919f34277fff3f6e6072492bce27d83b3e532eab
soytecnomago <62940560+soytecnomago@users.noreply.github.com>
2022-01-16 18:37:01 +0100

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "96b17f5156e2f0a2503e4e4491c0aaf0",
-"index.html": "0b0d8348e56c8d256d31477af71c89db",
-"/": "0b0d8348e56c8d256d31477af71c89db",
-"main.dart.js": "24886651e592f01faf3aebc1a538aa59",
+"index.html": "4a9926bf5f74b7cc6d882d7a67db719b",
+"/": "4a9926bf5f74b7cc6d882d7a67db719b",
+"main.dart.js": "11701f6cae7c268abee1de54bbc7039d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
